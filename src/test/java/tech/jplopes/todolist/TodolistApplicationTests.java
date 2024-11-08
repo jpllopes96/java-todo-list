d55ffe42f97849fa2111a959349e84223477f67a
jplopes <joaolacaia96@gmail.com>
2024-11-08 16:38:35 -0300

    starting project

---
+++ src/test/java/tech/jplopes/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jplopes.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
